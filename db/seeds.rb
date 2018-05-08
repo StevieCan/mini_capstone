917f7108fd9cf28d031d25534f487e35088b4544
Steve Han <shan61489@gmail.com>
2018-05-08 18:41:58 -0500

    added different products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "Lebron XV", price: 185 , image_url: "https://i2.wp.com/www.nicekicks.com/files/2018/03/nike-lebron-15-neon-1-1.jpg?fit=750%2C400&ssl=1", description: "The Lebron 15 is his most recent shoe released by Nike")
+product.save
+product = Product.new(name: "PG2", price: 120 , image_url: "https://cdn.shoepalace.com/product/images/960x/878627-417_01.jpg", description: "The Paul George 2 is a comfortable basketball shoe released by Nike")
+product.save
+product = Product.new(name: "Kyrie 3", price: 150 , image_url: "https://image.goat.com/crop/750/attachments/product_template_pictures/images/004/143/867/original/852395_103.png.png", description: "Kyrie 3 in white chrome released by Nike")
+product.save
+product = Product.new(name: "Dame 4", price: 80, image_url: "https://s7d2.scene7.com/is/image/dkscdn/17ADIMLLLRD4WHTBLPRF_White_Black_is/", description: "white and black Dame 4s are released by Adidas")
+product.save
+product = Product.new(name: "Kobe 9", price: 180 , image_url: "https://cdn.thesolesupplier.co.uk/2017/09/Nike-Kobe-9-EM-Dusty-Cactus1.png", description: "In my opinion, the most comfortable basketball shoes from Kobe Bryant and Nike")
+product.save
+product = Product.new(name: "KDX", price: 150 , image_url: "https://images.champssports.com/is/image/EBFL2/97815005_a1_ven_sc7?hei=1500&wid=1500", description: "The tenth basketball shoe released from Kevin Durant and Nike")
+product.save
