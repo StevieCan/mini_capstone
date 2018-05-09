94403dd606c107268f1804f61a6329714ab6bb4a
Steve Han <shan61489@gmail.com>
2018-05-08 19:29:14 -0500

    added methods and views to the mini-capstone

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,10 @@
 class Api::ProductsController < ApplicationController
+  def first_shoe_action
+    @product = Product.find(1)
+    render "first_shoe_action.json.jbuilder" 
+  end
+  def all_shoes_action
+    @products = Product.all
+    render "all_shoes_action.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-end
+  namespace :api do
+    get 'first_shoe_url' => 'products#first_shoe_action'
+    get 'all_shoes_url' => 'products#all_shoes_action'
+  end
+ end
