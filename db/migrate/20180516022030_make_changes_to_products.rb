6b4d69642d485de40dc8061c729b9c34d963a6b6
Steve Han <shan61489@gmail.com>
2018-05-16 21:27:52 -0500

    added migration

--- db/migrate/20180516022030_make_changes_to_products.rb
+++ db/migrate/20180516022030_make_changes_to_products.rb
@@ -1,9 +1,9 @@
 class MakeChangesToProducts < ActiveRecord::Migration[5.1]
   def change
-    change__column :products, :description, :text
+    change_column :products, :description, :text
 
-    change__column :products, :price, "numeric USING CAST(price AS numeric)"
-    change__column :products, :price, :decimal, precision: 9, scale: 2
+    change_column :products, :price, "numeric USING CAST(price AS numeric)"
+    change_column :products, :price, :decimal, precision: 9, scale: 2
 
     add_column :products, :in_stock, :boolean, default: true
   end
