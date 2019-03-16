3a740c65bda441a530e802f7baa085240356493f
Steve Han <shan61489@gmail.com>
2019-03-16 12:50:38 -0500

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
+Supplier.create!([
+  {name: "Eastbay", email: "eastbay@gmail.com", phone_number: "847-123-4567"},
+  {name: "Finish Line", email: "footlocker@gmail.com", phone_number: "847-765-4321"},
+  {name: "Foot Locker", email: "footlocker@gmail.com", phone_number: "847-777-7777"}
+])
+
+Product.create!([
+  {name: "Dame 4", price: "80.0", image_url: "https://s7d2.scene7.com/is/image/dkscdn/17ADIMLLLRD4WHTBLPRF_White_Black_is/", description: "white and black Dame 4s are released by Adidas", in_stock: true, supplier_id: 3},
+  {name: "Kobe 9", price: "180.0", image_url: "https://cdn.thesolesupplier.co.uk/2017/09/Nike-Kobe-9-EM-Dusty-Cactus1.png", description: "In my opinion, the most comfortable basketball shoes from Kobe Bryant and Nike", in_stock: true, supplier_id: 3},
+  {name: "PG2", price: "120.0", image_url: "https://cdn.shoepalace.com/product/images/960x/878627-417_01.jpg", description: "The Paul George 2 is a comfortable basketball shoe released by Nike", in_stock: true, supplier_id: 1},
+  {name: "Kyrie 3", price: "150.0", image_url: "https://image.goat.com/crop/750/attachments/product_template_pictures/images/004/143/867/original/852395_103.png.png", description: "Kyrie 3 in white chrome released by Nike", in_stock: true, supplier_id: 3},
+  {name: "Dame 4", price: "80.0", image_url: "https://s7d2.scene7.com/is/image/dkscdn/17ADIMLLLRD4WHTBLPRF_White_Black_is/", description: "white and black Dame 4s are released by Adidas", in_stock: true, supplier_id: 3},
+  {name: "Kobe 9", price: "180.0", image_url: "https://cdn.thesolesupplier.co.uk/2017/09/Nike-Kobe-9-EM-Dusty-Cactus1.png", description: "In my opinion, the most comfortable basketball shoes from Kobe Bryant and Nike", in_stock: true, supplier_id: 1},
+  {name: "Crazy Explosives", price: "120.0", image_url: "https://images.footlocker.com/pi/BW0931/zoom/adidas-crazy-explosive-pk-mens", description: "These shoes are crazy comfortable and are a collab from Andrew Wiggins of the Timberwolves and Adidas", in_stock: true, supplier_id: 3},
+  {name: "Kyrie 3", price: "200.0", image_url: "https://image.goat.com/crop/750/attachments/product_template_pictures/images/004/143/867/original/852395_103.png.png", description: "Kyrie 3 in white chrome released by Nike", in_stock: true, supplier_id: 2},
+  {name: "Lebron Soldiers", price: "95.0", image_url: "https://images.footlocker.com/pi/97644401/zoom/nike-lebron-soldier-11-mens", description: "These are the 11th model made from Lebron James and Nike. ", in_stock: true, supplier_id: 1},
+  {name: "Yeezy", price: "500.0", image_url: nil, description: "This is so overpriced", in_stock: true, supplier_id: 2},
+  {name: "Lebron XV", price: "200.0", image_url: "https://i2.wp.com/www.nicekicks.com/files/2018/03/nike-lebron-15-neon-1-1.jpg?fit=750%2C400&ssl=1", description: "The Lebron 15 is his most recent shoe released by Nike", in_stock: true, supplier_id: 1}
+])
+
+
+
 User.create!([
   {name: "Joe Cool", email: "joe@gmail.com", password_digest: "$2a$10$uFMXMyOjkkayxHUL4bIZRuGoDswXfxEX2vjuIUpW0kcaAr2oNBZHy", admin: false},
   {name: "Sue Everyone", email: "sue@gmail.com", password_digest: "$2a$10$zs7ZPv3bdtccaH3.6UQb9uEF3ebgwSz8Wye4TUMD7CCbyiGQKycKC", admin: false},
@@ -30,25 +52,9 @@ Image.create!([
 Order.create!([
   {user_id: 3, subtotal: nil, tax: nil, total: nil}
 ])
-Product.create!([
-  {name: "Dame 4", price: "80.0", image_url: "https://s7d2.scene7.com/is/image/dkscdn/17ADIMLLLRD4WHTBLPRF_White_Black_is/", description: "white and black Dame 4s are released by Adidas", in_stock: true, supplier_id: 3},
-  {name: "Kobe 9", price: "180.0", image_url: "https://cdn.thesolesupplier.co.uk/2017/09/Nike-Kobe-9-EM-Dusty-Cactus1.png", description: "In my opinion, the most comfortable basketball shoes from Kobe Bryant and Nike", in_stock: true, supplier_id: 3},
-  {name: "PG2", price: "120.0", image_url: "https://cdn.shoepalace.com/product/images/960x/878627-417_01.jpg", description: "The Paul George 2 is a comfortable basketball shoe released by Nike", in_stock: true, supplier_id: 1},
-  {name: "Kyrie 3", price: "150.0", image_url: "https://image.goat.com/crop/750/attachments/product_template_pictures/images/004/143/867/original/852395_103.png.png", description: "Kyrie 3 in white chrome released by Nike", in_stock: true, supplier_id: 3},
-  {name: "Dame 4", price: "80.0", image_url: "https://s7d2.scene7.com/is/image/dkscdn/17ADIMLLLRD4WHTBLPRF_White_Black_is/", description: "white and black Dame 4s are released by Adidas", in_stock: true, supplier_id: 3},
-  {name: "Kobe 9", price: "180.0", image_url: "https://cdn.thesolesupplier.co.uk/2017/09/Nike-Kobe-9-EM-Dusty-Cactus1.png", description: "In my opinion, the most comfortable basketball shoes from Kobe Bryant and Nike", in_stock: true, supplier_id: 1},
-  {name: "Crazy Explosives", price: "120.0", image_url: "https://images.footlocker.com/pi/BW0931/zoom/adidas-crazy-explosive-pk-mens", description: "These shoes are crazy comfortable and are a collab from Andrew Wiggins of the Timberwolves and Adidas", in_stock: true, supplier_id: 3},
-  {name: "Kyrie 3", price: "200.0", image_url: "https://image.goat.com/crop/750/attachments/product_template_pictures/images/004/143/867/original/852395_103.png.png", description: "Kyrie 3 in white chrome released by Nike", in_stock: true, supplier_id: 2},
-  {name: "Lebron Soldiers", price: "95.0", image_url: "https://images.footlocker.com/pi/97644401/zoom/nike-lebron-soldier-11-mens", description: "These are the 11th model made from Lebron James and Nike. ", in_stock: true, supplier_id: 1},
-  {name: "Yeezy", price: "500.0", image_url: nil, description: "This is so overpriced", in_stock: true, supplier_id: 2},
-  {name: "Lebron XV", price: "200.0", image_url: "https://i2.wp.com/www.nicekicks.com/files/2018/03/nike-lebron-15-neon-1-1.jpg?fit=750%2C400&ssl=1", description: "The Lebron 15 is his most recent shoe released by Nike", in_stock: true, supplier_id: 1}
-])
 
-Supplier.create!([
-  {name: "Eastbay", email: "eastbay@gmail.com", phone_number: "847-123-4567"},
-  {name: "Finish Line", email: "footlocker@gmail.com", phone_number: "847-765-4321"},
-  {name: "Foot Locker", email: "footlocker@gmail.com", phone_number: "847-777-7777"}
-])
+
+
 
 CartedProduct.create!([
   {user_id: 1, product_id: 5, quantity: 10, status: "removed", order_id: nil},
